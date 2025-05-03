b64630390dbb4b50d4346e5d8ec92c86d3db7f33
GabiZUU1 <dardaigabriel@gmail.com>
2025-05-04 01:36:23 +0300

    am incercat ceva

--- atestat4.0/atestat4.0/App.xaml.cs
+++ atestat4.0/atestat4.0/App.xaml.cs
@@ -9,15 +9,5 @@ namespace atestat4._0;
 /// </summary>
 public partial class App : Application
 {
-    private void Application_Startup(object sender, StartupEventArgs e)
-    {
-        MainWindow main = new MainWindow();
-        if(e.Args.Length == 1)
-        {
-            MessageBox.Show("Se deschide fisierul: " + e.Args[0]);
-            main.o
-        }
-        main.Show();
-    }
 }
 
